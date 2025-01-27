38ba2bb04b28d66e2a88131840a29069eacbf085
Shubham <2253shubham@gmail.com>
2025-01-26 23:42:27 -0500

    First Commit

---
+++ blind_75/Group_anagrams.py
@@ -0,0 +1,37 @@
+# problem link here https://leetcode.com/problems/group-anagrams/description/
+
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dicty = defaultdict(list)
+
+        for i in strs:
+            se = "".join(sorted(i))
+            dicty[se].append(i)
+
+        out = []
+        for i in dicty.values():
+            out.append(i)
+
+        return out
+    
+# Time Complexity = O(N * M logM), Space Complexity = O(N * M)
+# Where N is the length of list, m is the length of each word in list
+
+"""
+# Better approach
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
+
+# Time Complexity = O(N * M), Space Complexity = O(N)
+# Where N is the length of list, m is the length of each word in list
+
+"""
