60ff738b1731182be78311662217adbb6a526dcc
Shubham <2253shubham@gmail.com>
2025-01-30 23:00:01 -0500

    First commit

---
+++ blind_75/Reverse_linked_list.py
@@ -0,0 +1,20 @@
+# Problem description here
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = None
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
+        return node
+
+
+# Time Complexity = O(N), Space Complexity = O(1)
