d1f4b5053c6bf21825bd259630a04c209ab1c472
Shubham <2253shubham@gmail.com>
2025-02-03 00:30:27 -0500

    First commit

---
+++ blind_75/Longest_substring_without_repeat_chars.py
@@ -0,0 +1,19 @@
+# Problem description here https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        ch = set()
+        gmax = 0
+
+        for right in range(len(s)):
+            while s[right] in ch:
+                ch.remove(s[left])
+                left += 1
+            ch.add(s[right])
+            gmax = max(gmax, right - left + 1)
+        return gmax
+
+
+# Time complexity = O(N), Space Complexity = O(N)
