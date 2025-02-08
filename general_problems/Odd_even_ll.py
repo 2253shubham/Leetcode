4e6cd46f9eb934e8c1348182c35ed160703e9f93
Shubham <2253shubham@gmail.com>
2025-02-07 23:59:01 -0500

    First commit

---
+++ general_problems/Odd_even_ll.py
@@ -0,0 +1,27 @@
+# Problem description here https://leetcode.com/problems/odd-even-linked-list/description/
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        head1 = head
+        head2 = head.next
+        head3 = head.next
+
+        while head2 and head2.next:
+            head1.next = head1.next.next
+            head2.next = head2.next.next
+            head1 = head1.next
+            head2 = head2.next
+        head1.next = head3
+
+        return head
+
+
+# Time Complexity = O(N), Space Complexity = O(1)
