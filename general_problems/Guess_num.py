9dc78c44ff9ea156035690d6d853954d61bcaa3c
Shubham <2253shubham@gmail.com>
2025-02-16 23:24:21 -0500

    Added a better solution!

--- general_problems/Guess_num.py
+++ general_problems/Guess_num.py
@@ -1,5 +1,36 @@
 # Problem description here https://leetcode.com/problems/guess-number-higher-or-lower/description/
 
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        lower, upper = 1, n
+        while lower <= upper:
+            guess_num = (lower + upper) // 2
+            result = guess(guess_num)
+            if result == 0:
+                return guess_num
+            elif result == 1:
+                lower = guess_num + 1
+            else:
+                upper = guess_num - 1
+
+        return guess_num
+        
+# Time Complexity = O(logN), Space Complexity = O(1)
+
+
+"""
+
+# My original implementation (bit ineffective use of binary search)!
+
 # The guess API is already defined for you.
 # @param num, your guess
 # @return -1 if num is higher than the picked number
@@ -28,5 +59,5 @@ class Solution:
                 upper = guess_num
 
         return guess_num
-    
-# Time Complexity = O(logN), Space Complexity = O(1)
+
+"""
